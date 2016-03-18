70c83af29e392ebda91215ebed0b5fc896e0f8ad
maxwen <max.weninger@gmail.com>
2016-03-18 18:17:05 +0100

    Volume key music control [1/2]
    Change-Id: I3b1e72b95ec8b1b63ef0219259dfee77a5d339d3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4142,6 +4142,12 @@ public final class Settings {
          */
         public static final String FINGERPRINT_GESTURES_SWIPE_LEFT = "fingerprint_gestures_swipe_left";
 
+        /** Whether to skip music track with volume rocker
+        /**
+         * @hide
+         */
+        public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
+
         /**
          * Controls whether swipe right fingerprint gesture is enabled.
          * @hide
@@ -4447,6 +4453,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(KEY_CAMERA_LONG_PRESS_ACTION);
             PRIVATE_SETTINGS.add(KEY_CAMERA_DOUBLE_TAP_ACTION);
             PRIVATE_SETTINGS.add(SWAP_VOLUME_BUTTONS);
+            PRIVATE_SETTINGS.add(VOLUME_BUTTON_MUSIC_CONTROL);
         }
 
         /**
