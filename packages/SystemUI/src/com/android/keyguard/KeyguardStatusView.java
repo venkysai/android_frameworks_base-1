bcb7d9418bbd2ab8c1f6c523c28259ee8daa73f3
ezio84 <brabus84@gmail.com>
2018-05-27 14:48:03 +0200

    Lockscreen clock styles: update alarm status also if ambient is off
    when ambient display is disabled, setDark gets still called but
    mDarkAmount == darkAmount so it just returns without updating
    visibilities, but we still need to update them to get a refreshed
    alarm status
    
    Change-Id: I6938c1ab26ec04ea2c8dd59efe02bc0602d6891f

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -498,6 +498,7 @@ public class KeyguardStatusView extends GridLayout {
 
     public void setDark(float darkAmount) {
         if (mDarkAmount == darkAmount) {
+            updateVisibilities();
             return;
         }
         mDarkAmount = darkAmount;
@@ -524,7 +525,7 @@ public class KeyguardStatusView extends GridLayout {
         mAlarmStatusView.setCompoundDrawableTintList(ColorStateList.valueOf(blendedAlarmColor));
         mAnalogClockView.setDark(dark);
         mDeadPoolClockView.setDark(dark);
-        updateVisibilities();
+        updateVisibilities(); // with updated mDarkAmount value
     }
 
     public void setPulsing(boolean pulsing) {
